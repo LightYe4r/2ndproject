1b8a94798214947cb4d9bb09b78a356685cd9d93
LightYe4r <24adrenalin@naver.com>
2023-10-14 14:20:50 +0900

    FIX : HTTP_HOST

--- jari/jari/settings.py
+++ jari/jari/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY= os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["port-0-toy-k19y2kljwq5eju.sel4.cloudtype.app"]
+ALLOWED_HOSTS = ["https://port-0-toy-k19y2kljwq5eju.sel4.cloudtype.app"]
 
 
 # Application definition
