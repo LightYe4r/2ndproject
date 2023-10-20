8fafe502c3b096de2b02af8db71c8eff23232f29
LightYe4r <24adrenalin@naver.com>
2023-10-20 19:54:04 +0900

    DEL : SECRET_KEY

--- jari/jari/settings.py
+++ jari/jari/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3gs+nh97@$6u59ah0_dcwh^!eua1w6&x&8@&cl4_h)0k!cn8^q'
-# SECRET_KEY= os.environ['SECRET_KEY']
+
+SECRET_KEY= os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
