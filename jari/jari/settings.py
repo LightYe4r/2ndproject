ff4e8b18d795527f21124fed1ac95b992512acda
LightYe4r <24adrenalin@naver.com>
2023-10-31 01:24:35 +0900

    FIX : token life time

--- jari/jari/settings.py
+++ jari/jari/settings.py
@@ -69,7 +69,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
     'SLIDING_TOKEN_LIFETIME': timedelta(days=1),
     'SLIDING_TOKEN_REFRESH_LIFETIME_LIFETIME': True,
